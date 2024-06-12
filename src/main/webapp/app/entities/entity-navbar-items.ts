7ac3044720964e9efe2abe5e8512b0dedb452bbe
tmetn <stanermetin@gmail.com>
2024-06-12 11:08:07 +0200

    change logo

--- src/main/webapp/app/entities/entity-navbar-items.ts
+++ src/main/webapp/app/entities/entity-navbar-items.ts
@@ -1,3 +1,13 @@
 import NavbarItem from 'app/layouts/navbar/navbar-item.model';
 
 export const EntityNavbarItems: NavbarItem[] = [];
+
+// Define your new entity
+const newEntity: NavbarItem = {
+  name: 'New Entity',
+  route: '/new-entity-link',
+  translationKey: 'New Entity Label',
+};
+
+// Add the new entity to the array
+EntityNavbarItems.push(newEntity);
